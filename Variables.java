3c6b9a556d291d8d057831e39c3985602b597e25
oksart <73427327+oksart@users.noreply.github.com>
2020-10-25 16:32:01 +0300

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.hw1;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte a=1;
+        short b=129;
+        int c=33000;
+        long d=2150000000L;
+        float e=12.56f;
+        double f=123.546;
+        char g='%';
+        boolean h=true;
+    }
+}
